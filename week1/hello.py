1558b52d4182176a91936ab9878a0e8bf38dc7a0
machinelearing <olooyasir5@gmail.com>
2026-07-21 21:27:27 +0000

    Adding codes to my hello.py

--- week1/hello.py
+++ week1/hello.py
@@ -1,5 +1,5 @@
 
-"""
+""""
 Week 1 · Lab 1.1 — My First Python Script
 =========================================
 Follow the lab on the course dashboard, and write your code here.
@@ -8,9 +8,14 @@ Run it with:   python week1/hello.py
 """
 
 # TODO: print a greeting
+print("Good evening everyone!")
 
 
 # TODO: create a variable with your name, and use it in a message
+yas = "Yasir Abdul-Rahaman"
+
+print(f"My name is {yas}.")
 
 
 # TODO: try an f-string  ->  print(f"Hello, {name}!")
+print(f"Hello, {yas}! you welcome to the world of Python programming.")
